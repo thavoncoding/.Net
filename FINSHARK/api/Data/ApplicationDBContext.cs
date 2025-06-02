e8e746ce76ebb34a81f9d62ea0d35985de982511
thavoncoding <stongchhorvon8@gmail.com>
2025-06-02 15:51:29 +0700

    Initial commit

---
+++ FINSHARK/api/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
